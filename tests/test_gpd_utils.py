0fef409658673e75960722028e320d9e3562ce27
disbr007 <disbr007@umn.edu>
2021-10-01 13:05:17 -0400

    fix test data location

--- tests/test_gpd_utils.py
+++ tests/test_gpd_utils.py
@@ -1,15 +1,15 @@
 import json
 import unittest
+from pathlib import Path
 import pytest_check as check
 import sys
 import os
 
 import geopandas as gpd
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from geotools.gpdtools import esri2gdf
 
-ESRIJSON_EXAMPLE = r'/home/jeff/scratch/esrijson_example.json'
+ESRIJSON_EXAMPLE = Path(__file__).parent / r'test_data/esrijson_example.json'
 
 
 class TestEsri2GDF(unittest.TestCase):
