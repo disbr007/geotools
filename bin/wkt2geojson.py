a87db730cc7487e231c3635e66ce88bf43967439
disbr007 <disbr007@umn.edu>
2021-10-25 16:59:05 -0400

    test_file2geojson

--- bin/wkt2geojson.py
+++ bin/wkt2geojson.py
@@ -1,9 +1,14 @@
 import argparse
+import json
 import os
 
 import shapely
 import geopandas as gpd
 
+
+PARAMATER = 'parameter'
+LOCATION = 'location'
+WKT = 'wkt'
 
 def wkt2geojson(wkt_str, geojson_out, crs='epsg:4326'):
     print(f'Converting wkt to geojson: {geojson_out}')
@@ -25,6 +30,14 @@ def geojson2wkt(geojson, wkt_out = None):
                 out.write(w)
                 out.write('\n')
 
+
+def test_file2geojson(test_file, geojson_out):
+    print(f'Converting test file to geojson: {test_file}')
+    with open(test_file, 'r') as src:
+        data = json.load(src)
+    wkt = data[PARAMATER][LOCATION][WKT]
+    wkt2geojson(wkt_str=wkt, geojson_out=geojson_out)
+        
     
 def main(args):
     if args.input_wkt:
@@ -37,6 +50,8 @@ def main(args):
         wkt2geojson(input_wkt, args.out_geojson)
     if args.input_geojson:
         geojson2wkt(args.input_geojson, args.out_wkt)
+    if args.input_test_file:
+        test_file2geojson(args.input_test_file, args.out_geojson)
 
         
 if __name__ == '__main__':
@@ -44,6 +59,8 @@ if __name__ == '__main__':
 
     parser.add_argument('-g', '--input_geojson', type=os.path.abspath)
     parser.add_argument('-og', '--out_geojson', type=os.path.abspath)
+    
+    parser.add_argument('-it', '--input_test_file', type=os.path.abspath)
     
     parser.add_argument('-wkt', '--input_wkt', type=str)
     parser.add_argument('-ow', '--out_wkt', type=os.path.abspath)
