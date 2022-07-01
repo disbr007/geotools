6cd0114d795e6420925a042e717bd8d4993ef27d
disbr007 <disbr007@umn.edu>
2022-07-01 11:12:51 -0400

    add relative path creation in destination directory

--- bin/batch_gdal.py
+++ bin/batch_gdal.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 import subprocess
 from subprocess import PIPE
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 from tqdm import tqdm
 
@@ -42,6 +42,15 @@ def get_files(source_dirs: list, ext: str) -> List[Path]:
     return files
 
 
+def get_files_with_sources(source_dirs: list, ext: str) -> Dict[Path, List[Path]]:
+    files = {sd: [] for sd in source_dirs}
+    for sd in source_dirs:
+        sd_files = list(Path(sd).rglob(f'*{ext}'))
+        files[sd].extend(sd_files)
+    # logger.info(f'Files found: {sum([len(file_list) for file_list in files.values()])}')
+    return files
+
+
 def compute_statistics(files: List[Path], dryrun: bool = False):
     for in_raster in (pbar := tqdm(files)):
         # Compute statistics
@@ -49,6 +58,8 @@ def compute_statistics(files: List[Path], dryrun: bool = False):
         pbar.set_description(f'Calculatings stats - {in_raster.name}')
         if not dryrun:
             run_subprocess(stats_cmd)
+        else:
+            logger.debug(stats_cmd)
 
 
 def build_overviews(files: List[Path], dryrun=False):
@@ -58,21 +69,32 @@ def build_overviews(files: List[Path], dryrun=False):
         pbar.set_description(f'Creating overviews - {in_raster.name}')
         if not dryrun:
             run_subprocess(overviews_cmd)
+        else:
+            logger.debug(overviews_cmd)
     
 
-def create_tiles(files: List[Path], 
+def create_tiles(source_dirs: list, 
+                 ext: str,
                  out_dir: Path, 
                  compress_method: Optional[str] = "LZW", 
                  dryrun=False):
     '''Create tiles optionally with compression'''
-    for in_raster in (pbar := tqdm(files)):
-        pbar.set_description(f'Creating overviews - {in_raster.name}')
-        out_raster = out_dir / in_raster.name
-        tile_cmd = f"gdal_translate {str(in_raster)} {out_raster} -co TILED=YES -co COPY_SRC_OVERVIEWS=YES"
-        if compress_method is not None:
-            tile_cmd += f" -co COMPRESS={compress_method}"
-        if not dryrun:
-            run_subprocess(tile_cmd)
+    sources_files = get_files_with_sources(source_dirs, ext)
+    pbar = tqdm(total=sum([len(file_list) for file_list in sources_files.values()]))
+    for source_dir, file_list in sources_files.items():
+        for in_raster in file_list:
+            pbar.set_description(f'Creating overviews - {in_raster.name}')
+            out_raster = out_dir / in_raster.relative_to(source_dir)
+            # Make any required directories
+            out_raster.parent.mkdir(parents=True, exist_ok=True)
+            tile_cmd = f"gdal_translate {str(in_raster)} {str(out_raster)} -co TILED=YES -co COPY_SRC_OVERVIEWS=YES"
+            if compress_method is not None:
+                tile_cmd += f" -co COMPRESS={compress_method}"
+            if not dryrun:
+                run_subprocess(tile_cmd)
+            else:
+                logger.debug(tile_cmd)
+            pbar.update(1)
 
     
 def batch_gdal(source_dirs: List[str],
@@ -90,9 +112,12 @@ def batch_gdal(source_dirs: List[str],
     if tiles:
         if not out_tiles_dir:
             raise argparse.ArgumentError('Must provide out_tiles_dir when creating tiles.')
-        create_tiles(files, out_dir=out_tiles_dir, dryrun=dryrun)
+        create_tiles(source_dirs=source_dirs,
+                     ext=ext,
+                     out_dir=out_tiles_dir, 
+                     dryrun=dryrun)
     logger.info('Done.')    
-    
+
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -105,6 +130,8 @@ if __name__ == '__main__':
     parser.add_argument('--tiles', action='store_true')
     parser.add_argument('--out_tiles_dir',
                         help='Path to write tiled rasters to - cannot be created in place.')
+    parser.add_argument('--verbose', action='store_true',
+                        help='Set log level to DEBUG')
     parser.add_argument('--dryrun', action='store_true')
     
     args = parser.parse_args()
@@ -112,6 +139,13 @@ if __name__ == '__main__':
     # Validate arguments
     if args.tiles and args.out_tiles_dir is None:
         parser.error("--tiles requires --out_tiles_dir")
+    
+    if args.verbose:
+        level = logging.DEBUG
+        logger = logging.getLogger(__name__)
+        logger.setLevel(level)
+        for handler in logger.handlers:
+            handler.setLevel(level)
     
     batch_gdal(source_dirs=args.source_dir,
                ext=args.ext,
